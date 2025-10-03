e64d6c0a399847c5e2181993f4557bae467b8a07
ShichiNoBushi <shichinobushi@gmail.com>
2025-10-03 15:15:46 -0500

    Added persistence with saving/loading using JSON.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 class To_Do_Item:
     def __init__(self, tid, task, start, target):
@@ -25,6 +26,27 @@ class To_Do_Item:
     def finish(self):
         self.finished = True
 
+    def to_dict(self):
+        return {
+            "tid": self.tid,
+            "task": self.task,
+            "start": self.start.isoformat(),
+            "target": self.target.isoformat(),
+            "finished": self.finished
+        }
+    
+    @classmethod
+    def from_dict(cls, data):
+        item = cls(
+            tid = data["tid"],
+            task = data["task"],
+            start = datetime.fromisoformat(data["start"]),
+            target = datetime.fromisoformat(data["target"])
+        )
+        item.finished = data.get("finished", False)
+
+        return item
+
 def to_do_add(td_list, item):
     td_list[item.tid] = item
 
@@ -47,19 +69,60 @@ def to_do_print(td_list, pending_only = False, finished_only = False):
         elif not pending_only and not finished_only:
             print(i)
 
+def to_json(td_list):
+    json_safe_list = [item.to_dict() for item in td_list.values()]
+
+    return json.dumps(json_safe_list, indent = 4)
+
+def from_json(json_str):
+    json_safe_list = json.loads(json_str)
+
+    return {int(tid): To_Do_Item.from_dict(item) for tid, item in json_safe_list}
+
+def save_list(td_list, filename = "tasks.json"):
+    with open(filename, "w") as f:
+        json.dump([item.to_dict() for item in td_list.values()], f, indent = 4)
+
+def load_list(filename = "tasks.json"):
+    try:
+        with open(filename, "r") as f:
+            data = json.load(f)
+        return {item["tid"]: To_Do_Item.from_dict(item) for item in data}
+    except FileNotFoundError:
+        return {}
+    except json.JSONDecodeError:
+        return {}
+    
+def reset_file(filename = "tasks.json"):
+    open(filename, "w").close()
+
 def main():
-    to_do_list = {}
+    to_do_list = load_list()
+
+    print("Loading list:")
+    to_do_print(to_do_list)
+
+    print("\nResetting file:")
+    reset_file()
+    to_do_list = load_list()
+    to_do_print(to_do_list)
 
     to_do_add(to_do_list, To_Do_Item(0, "Buy groceries", datetime.strptime("2025 Oct 2", "%Y %b %d"), datetime.strptime("2025 Oct 3", "%Y %b %d")))
     to_do_add(to_do_list, To_Do_Item(1, "Call Mom", datetime.strptime("2025 Oct 1", "%Y %b %d"), datetime.strptime("2025 Oct 2", "%Y %b %d")))
     to_do_add(to_do_list, To_Do_Item(2, "Finish Python project", datetime.strptime("2025 Oct 1", "%Y %b %d"), datetime.strptime("2025 Oct 31", "%Y %b %d")))
 
-    print("Full list:")
+    print("\nFull list:")
     to_do_print(to_do_list)
 
     to_do_list[1].finish()
 
     print("\nAfter one task finished:")
+    to_do_print(to_do_list)
+
+    print("\nTo JSON:")
+    print(to_json(to_do_list))
+
+    print("\nFrom JSON:")
     to_do_print(to_do_list)
 
     print("\nFilter pending only:")
@@ -72,6 +135,9 @@ def main():
 
     print("\nRemoved one:")
     to_do_print(to_do_list)
+
+    print("\nSaving list...")
+    save_list(to_do_list)
 
 if __name__ == "__main__":
     main()
